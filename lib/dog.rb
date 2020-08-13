252f28287b9d0fcd7ff11bfbb7d1352959beb4a1
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2020-08-13 23:58:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,30 @@ class Dog
   
   attr_accessor :name
   
-  def initialize(name)
-    @
+    def initialize(name)
+    @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = [] 
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+end
+
+  def save
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+end
